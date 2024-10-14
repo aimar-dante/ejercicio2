e9424e4ff143ed1aec41b493ddca6ba5fe14ac83
2AW3 User 20 <2AW3-20@yourdomain.com>
2024-10-14 12:58:30 +0200

    armas

---
+++ controlador/ver_armas.php
@@ -0,0 +1,14 @@
+<?php
+require(__DIR__ . '/../modelo/MArmas.php');
+
+
+require_once(__DIR__ . '/../vista/VArmas.php');
+
+$con= new Marmas();
+$armas = $con->select_armas();
+
+$vista = new VArmas;
+$vista->inithtml();
+$vista->tabla_armas($armas);
+$vista->endhtml();
+?>
